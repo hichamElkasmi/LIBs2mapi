7b78e9b06ee010650e56bbc3f130636f6903fbc2
hichamElkasmi <hicham.elkasmi1@gmail.com>
2024-10-10 12:15:06 +0100

    DELETE METHOD GET

--- src/main/java/com/s2m/msr/sswallet/MsrtEntry.java
+++ src/main/java/com/s2m/msr/sswallet/MsrtEntry.java
@@ -10,8 +10,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
-import javax.ws.rs.GET;
-
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
